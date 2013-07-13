f835ce9694b3ddbfb44062be1e03acb7f0e912d8
Sirius Dely <siriusdely@icloud.com>
2013-07-13 09:52:25 +0000

    [+] bootstrap assets [~] before really using get_absolute_url from Post model

--- microblog/settings/base.py
+++ microblog/settings/base.py
@@ -47,7 +47,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = root("..", "uploads")
+MEDIA_ROOT = root("..", "..", "uploads")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -58,7 +58,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = root("..", "static")
+STATIC_ROOT = root("..", "..", "static")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -69,7 +69,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    root("assets"),
+    root("..", "assets"),
 )
 
 # List of finder classes that know how to find static files in
