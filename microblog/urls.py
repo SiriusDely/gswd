83369c0717d38fc75efa605df620721884233b21
Sirius Dely <siriusdely@icloud.com>
2013-07-13 10:20:07 +0000

    [^] move settings STATIC_ROOT & MEDIA_ROOT up one level closer [^] add static's urlpatterns to microblog's urls.py

--- microblog/urls.py
+++ microblog/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
@@ -19,4 +20,10 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('',
+	(r'^static/(?P.*)$', 'django.views.static.serve', 
+		{'document_root': settings.STATIC_ROOT}
+	),
 )
