b41b80dafd29da0955419bd17387ba8017177486
Sirius Dely <siriusdely@icloud.com>
2013-07-13 09:38:23 +0000

    [^] base template, blog views, templates & urls

--- blog/views.py
+++ blog/views.py
@@ -1,39 +1,15 @@
 # Create your views here.
-from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
 from .models import Post
 
-def blog_list(request, *args, **kwargs):
-	post_list = Post.objects.filter(published=True)
-	template_name = "blog/post_list.html"
-
-	context = {
-		"post_list" : post_list
-	}
-
-	return render(request, template_name, context)
-
-class PostListView(ListView):
-	model = Post
-
+class PublishPostsMixin(object):
 	def get_queryset(self):
-		queryset = super(PostListView, self).get_queryset()
+		queryset = super(PublishPostsMixin, self).get_queryset()
 		return queryset.filter(published=True)
 
-def blog_detail(request, pk, *args, **kwargs):
-	post = Post.objects.get(pk=pk, published=True)
-	template_name = "blog/post_detail.html"
-
-	context = {
-		"post" : post
-	}
-
-	return render(request, template_name, context)
-
-class PostDetailView(DetailView):
+class PostListView(PublishPostsMixin, ListView):
 	model = Post
 
-	def get_queryset(self):
-		queryset = super(PostDetailView, self).get_queryset()
-		return queryset.filter(published=True)
+class PostDetailView(PublishPostsMixin, DetailView):
+	model = Post
