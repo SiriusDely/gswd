7e02c0fdf9ea8c7345031861dfce5f5b7c072d7c
Sirius Dely <siriusdely@icloud.com>
2013-07-13 06:19:47 +0000

    [+] 500.html & index.html & root url to index.html

--- microblog/urls.py
+++ microblog/urls.py
@@ -2,12 +2,16 @@ from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
+from django.views.generic import TemplateView
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'microblog.views.home', name='home'),
     # url(r'^microblog/', include('microblog.foo.urls')),
+    url(r"^$", TemplateView.as_view(template_name="index.html")),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
